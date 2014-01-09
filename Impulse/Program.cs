9faf8a16e644ae79f8989e02b662390f1c6719b1
Tomek K. <tomicode@gmail.com>
2014-01-09 17:03:17 +0100

    Initial file layout.

---
+++ Impulse/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Impulse
+{
+    class Program
+    {
+        static Lexer lex = new Lexer();
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
