a481141fe04b73c3416390ecae8803ce7cc93787
Tomek K. <tomicode@gmail.com>
2014-01-11 12:42:45 +0100

    New token types.

--- Impulse/Parsing/Lexer.cs
+++ Impulse/Parsing/Lexer.cs
@@ -14,7 +14,9 @@ namespace Impulse
         Token_Chars = 2,
         Token_Keyword = 3,
         Token_Variable = 4,
-        Token_Operator = 5
+        Token_Operator = 5,
+        Token_Brackets = 6,
+        Token_Decimal = 7
     }
 
     struct Token
@@ -29,18 +31,19 @@ namespace Impulse
 
         public Token[] LexTextStream(StreamReader stream)
         {
-            char[] nextChar = new char[1];
+            char nextChar;
             char[] buffer = new char[128];
 
             Token[] tokens = new Token[256]; // Only for now.
             int lexPosition = 0;
 
-            while (stream.Read(nextChar, 0, 1) == 1)
+            while ((nextChar = (char)stream.Read()) != 0)
             {
                 if (state == TokenState.Token_String || state == TokenState.Token_Chars)
                 {
-                    if (nextChar[0] == '"' && state == TokenState.Token_String 
-                        || nextChar[0] == '\'' && state == TokenState.Token_Chars)
+                    if (nextChar == '\n') return null; // Exception! Someone does not closed the string!
+                    if (nextChar == '"' && state == TokenState.Token_String 
+                        || nextChar == '\'' && state == TokenState.Token_Chars)
                     {
                         int tokenPos = getTokenTableIndex(tokens);
                         if (tokenPos == -1) return null; // Exception! But not now xD
@@ -55,40 +58,75 @@ namespace Impulse
                     }
                     else
                     {
-                        buffer[lexPosition] = nextChar[0];
+                        buffer[lexPosition] = nextChar;
                         lexPosition++;
                     }
                 }
                 else if (state == TokenState.Token_Variable || state == TokenState.Token_Keyword)
                 {
+                    if(nextChar == ' '
+                        || nextChar == '('
+                        || nextChar == '\n'
+                        || nextChar == '='
+                        || nextChar == '+'
+                        || nextChar == '<'
+                        || nextChar == '>' 
+                        || nextChar == '*'
+                        || nextChar == '/')
+                    {
+                        int tokenPos = getTokenTableIndex(tokens);
+                        if (tokenPos == -1) return null; // Buffer overflow xD
 
+                        tokens[tokenPos] = new Token();
+                        tokens[tokenPos].token = bufferToStringToken(buffer, lexPosition);
+                        tokens[tokenPos].type = state;
+
+                        if(nextChar == '='
+                            || nextChar == '+'
+                            || nextChar == '*'
+                            || nextChar == '/'
+                            || nextChar == '>'
+                            || nextChar == '<')
+                        {
+                            state = TokenState.Token_Operator;
+                            buffer[0] = nextChar;
+                            lexPosition = 1;
+                            continue;
+                        }
+                        else if(nextChar == '(')
+                        {
+                            state = TokenState.Token_Brackets;
+                        }
+                        else
+                        {
+                            state = TokenState.Token_Unknown;
+                        }
+                        lexPosition = 0;
+                        continue;
+                    }
                 }
-                else if (nextChar[0] == '"' && state == TokenState.Token_Unknown)
+                else if (nextChar == '"' && state == TokenState.Token_Unknown)
                 {
                     state = TokenState.Token_String;
                     continue;
                 }
-                else if (nextChar[0] == '\'')
+                else if (nextChar == '\'')
                 {
                     state = TokenState.Token_Chars;
                     continue;
                 }
-                else if (nextChar[0] == '$')
+                else if (nextChar == '!')
                 {
                     state = TokenState.Token_Variable;
                     continue;
                 }
-                else if (nextChar[0] >= 'a' 
-                        || nextChar[0] <= 'z'
-                        || nextChar[0] >= 'A'
-                        || nextChar[0] <= 'Z' )
+                else if (nextChar >= 'a' 
+                        || nextChar <= 'z'
+                        || nextChar >= 'A'
+                        || nextChar <= 'Z' )
                 {
                     state = TokenState.Token_Keyword;
                     continue;
-                }
-                else if (nextChar[0] == ' ')
-                {
-
                 }
             }
 
